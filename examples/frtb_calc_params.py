6fb39d93e66d548c9629f2f62ecc0c0e0fea92ff
AnatolyBuga <60788447+AnatolyBuga@users.noreply.github.com>
2023-01-19 15:07:46 +0000

    Deploying to gh-pages from @ ultima-ib/ultibi-frtb-book.github.io@a07f1e205fe2ba3a935cffa03c000d2d29b54da0 🚀

---
+++ examples/frtb_calc_params.py
@@ -0,0 +1,177 @@
+import ultibi as ul
+import json
+
+ds = ul.FRTBDataSet.from_config_path("./data/frtb/datasource_config.toml")
+ds.prepare()
+for calc_param in ds.calc_params:
+    if "com_delta_" in calc_param["name"]:
+        # print(calc_param) #<-- uncomment to see
+        pass
+
+com_delta_diff_cty_rho_per_bucket_base = json.dumps(
+    [1.55, 1.95, 1.4, 1.8, 1.6, 1.65, 1.55, 1.45, 1.15, 1.4, 1.15]
+)
+com_delta_gamma_low = json.dumps(
+    {
+        "v": 1,
+        "dim": [11, 11],
+        "data": [
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            1.2,
+            0.0,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            1.2,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ],
+    }
+)
+
+calc_params = dict(
+    com_delta_diff_cty_rho_per_bucket_base=com_delta_diff_cty_rho_per_bucket_base,
+    com_delta_gamma_low=com_delta_gamma_low,
+)
+print(calc_params)
+
+request1 = dict(
+    measures=[
+        ["Commodity DeltaCharge Low", "scalar"],
+        ["Commodity DeltaCharge Medium", "scalar"],
+        ["Commodity DeltaCharge High", "scalar"],
+    ],
+    groupby=["Group"],
+    hide_zeros=True,
+    calc_params=calc_params,
+)
+
+result1 = ds.execute(request1)
+
+# print(result1) #<-- uncomment to see
+
+request2 = dict(
+    measures=[
+        ["Commodity DeltaCharge Low", "scalar"],
+        ["Commodity DeltaCharge Medium", "scalar"],
+        ["Commodity DeltaCharge High", "scalar"],
+    ],
+    groupby=["Group"],
+    hide_zeros=True,
+)
+
+result2 = ds.execute(request2)
+
+# print(result2) #<-- uncomment to see
